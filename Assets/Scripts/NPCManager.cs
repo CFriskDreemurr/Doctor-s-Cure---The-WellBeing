1ff796f1fafa430aa14d9a96058164c9c31c4c24
Frisk Dreemurr <capis1134@gmail.com>
2026-03-30 00:21:15 +0200

    npc managing pt1

--- Assets/Scripts/NPCManager.cs
+++ Assets/Scripts/NPCManager.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class NPCManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject windowSpot1;
+    [SerializeField] private GameObject windowSpot2;
+    [SerializeField] private GameObject windowSpot3;
+    [SerializeField] private GameObject evaluationSpot;
+    [SerializeField] private GameObject infirmarySpot;
+    [SerializeField] private GameObject infirmarySpot2;
+    [SerializeField] private GameObject infirmarySpot3;
+    [SerializeField] private GameObject operatingSpot;
+    [SerializeField] private WindowQueue windowQueuee;
+
+
+    private void Start()
     {
-        
+        SpriteRenderer windowSprite1 = windowSpot1.GetComponent<SpriteRenderer>();
+        SpriteRenderer windowSprite2 = windowSpot2.GetComponent<SpriteRenderer>();
+        SpriteRenderer windowSprite3 = windowSpot3.GetComponent<SpriteRenderer>();
+        SpriteRenderer evaluationSprite = evaluationspot.GetComponent<SpriteRenderer>();
+    }
+    public void WindowToEvaluation()
+    {
+        GameObject objectToMove = windowQueuee.windowQueue.Peek();
+        if (objectToMove != null)
+        {
+            GameObject[] queueArray = windowQueuee.windowQueue.ToArray();
+            
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
