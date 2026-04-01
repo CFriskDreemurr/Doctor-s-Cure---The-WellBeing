88716ae499acf47742f8b55ab1575af90cd8dfe3
Frisk Dreemurr <capis1134@gmail.com>
2026-04-01 13:00:52 +0200

    Time Manager
    and come crazy optimizations

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class TimeManager : MonoBehaviour
@@ -6,33 +7,47 @@ public class TimeManager : MonoBehaviour
     [SerializeField] private DayQueue day1Queue;
     [SerializeField] private DayQueue day2Queue;
     [SerializeField] private WindowQueue daQueue;
-    [SerializeField] private GameObject windowSpot1;
-    [SerializeField] private GameObject windowSpot2;
-    [SerializeField] private GameObject windowSpot3;
-    private SpriteRenderer windowSprite1;
-    private SpriteRenderer windowSprite2;
-    private SpriteRenderer windowSprite3;
+    [SerializeField] private int nrOfCycles;
+    [SerializeField] private float cycleLenght;
+    private int daCounter;
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         todayQueue = day1Queue;
-        windowSprite1 = windowSpot1.GetComponent<SpriteRenderer>();
-        windowSprite2 = windowSpot2.GetComponent<SpriteRenderer>();
-        windowSprite3 = windowSpot3.GetComponent<SpriteRenderer>();
         StartDay();
+        
     }
     public void StartDay()
     {
-        foreach(NPC e in todayQueue.dailyQueue){
-            daQueue.AddToQueue(e);
-        }
-        windowSprite1.sprite = todayQueue.dailyQueue[0].NPCSprites[0];
-        windowSprite2.sprite = todayQueue.dailyQueue[1].NPCSprites[0];
-        windowSprite3.sprite = todayQueue.dailyQueue[2].NPCSprites[0];
+        daQueue.AddToQueue(todayQueue.dailyQueue[0]);
+        daQueue.AddToQueue(todayQueue.dailyQueue[1]);
+        daQueue.AddToQueue(todayQueue.dailyQueue[2]);
+        daCounter = 3;
+        StartCoroutine(DayCycle());
 
     }
-
-    
+    public void EndDay()
+    {
+        Debug.Log("Da Day Haths Ended.");
+    }
+    IEnumerator DayCycle()
+    {
+        for (int i = 0; i < nrOfCycles; i++)
+        {
+            yield return new WaitForSeconds(cycleLenght);
+            if (daCounter < todayQueue.dailyQueue.Count)
+            {
+                daQueue.AddToQueue(todayQueue.dailyQueue[daCounter]);
+            }
+            else
+            {
+                Debug.Log("hihi");
+            }
+                daCounter += 1;
+        }
+        EndDay();
+    }
 }
