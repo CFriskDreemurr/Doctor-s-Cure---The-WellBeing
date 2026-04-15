811f85069b145b6c5583bf3ce68aafeb1d8a5b65
Frisk Dreemurr <capis1134@gmail.com>
2026-04-15 16:02:05 +0200

    fixed clickable thingz

--- Assets/Scripts/JournalManager.cs
+++ Assets/Scripts/JournalManager.cs
@@ -20,23 +20,35 @@ public class JournalManager : MonoBehaviour
     }
     public void NewPage()
     {
-        if (illnessList.illnessList[0+(pgNr*6)] != null)
+        slot1.gameObject.SetActive(false);
+        slot2.gameObject.SetActive(false);
+        slot3.gameObject.SetActive(false);
+        slot4.gameObject.SetActive(false);
+        slot5.gameObject.SetActive(false);
+        slot6.gameObject.SetActive(false);
+        if (illnessList.illnessList.Count >= (1 + (pgNr * 6)))
         {
+            slot1.gameObject.SetActive(true);
             slot1.NewIllness(illnessList.illnessList[0 + (pgNr * 6)]);
-            if (illnessList.illnessList[1 + (pgNr * 6)] != null)
+            if (illnessList.illnessList.Count >= (2 + (pgNr * 6)))
             {
+                slot2.gameObject.SetActive(true);
                 slot2.NewIllness(illnessList.illnessList[1 + (pgNr * 6)]);
-                if (illnessList.illnessList[2 + (pgNr * 6)] != null)
+                if (illnessList.illnessList.Count >= (3 + (pgNr * 6)))
                 {
+                    slot3.gameObject.SetActive(true);
                     slot3.NewIllness(illnessList.illnessList[2 + (pgNr * 6)]);
-                    if (illnessList.illnessList[3 + (pgNr * 6)] != null)
+                    if (illnessList.illnessList.Count >= (4 + (pgNr * 6)))
                     {
+                        slot4.gameObject.SetActive(true);
                         slot4.NewIllness(illnessList.illnessList[3 + (pgNr * 6)]);
-                        if (illnessList.illnessList[4 + (pgNr * 6)] != null)
+                        if (illnessList.illnessList.Count >= (5 + (pgNr * 6)))
                         {
+                            slot5.gameObject.SetActive(true);
                             slot5.NewIllness(illnessList.illnessList[4 + (pgNr * 6)]);
-                            if (illnessList.illnessList[5 + (pgNr * 6)] != null)
+                            if (illnessList.illnessList.Count >= (6+(pgNr*6)))
                             {
+                                slot6.gameObject.SetActive(true);
                                 slot6.NewIllness(illnessList.illnessList[5 + (pgNr * 6)]);
                             }
                         }
